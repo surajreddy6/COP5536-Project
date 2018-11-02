dfd22842a2cdd0cbdc030a4fbd535a270156c695
unknown <sthanugundla@ufl.edu>
2018-11-02 18:47:12 -0400

    cascading cut

--- src/fibheap/FibonacciHeap.java
+++ src/fibheap/FibonacciHeap.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class FibonacciHeap {
     // The node that contains the max key
     private Node max;
-    // Hash table with key-keyword and value-node of the keyword
+    // Hash table with key -> keyword and value -> node of the keyword
     private HashMap<String, Node> hashTable = new HashMap<String, Node>();
 
     public void insert(String keyword, int frequency) {
@@ -40,27 +40,60 @@ public class FibonacciHeap {
             max.getRightSibling().setLeftSibling(node);
             // set node as right sibling of max
             max.setRightSibling(node);
+            // childCut is not defined for the root but set it to false
+            node.setChildCut(false);
             // if the incoming node's count is greater set is as max
-            if(max.getCount() < node.getCount()) {
+            if(node.getCount() > max.getCount()) {
                 max = node;
             }
         }
     }
 
     public void increaseKey(Node node, int frequency) {
-        // if the node is a root node just increase the count and check with max
+        // update count of node
+        node.setCount(node.getCount() + frequency);
+        // if the node is a root node check with max
         if(node.getParent() == null) {
-            node.setCount(node.getCount() + frequency);
             if(node.getCount() > max.getCount()) {
                 max = node;
             }
         } else {
             // if count of node becomes greater than the parent remove the
-            // subtree rooted and node and re-insert into the heap
-            if(node.getCount() + frequency > node.getParent().getCount()) {
+            // node (and it's subtree) and re-insert into the heap
+            if(node.getCount() > node.getParent().getCount()) {
+                removeAndReinsertNode(node);
+            }
+        }
+    }
 
+    // Never called on a root node. Always called on a node with a parent
+    public void removeAndReinsertNode(Node node) {
+        // parent of the node
+        Node parent = node.getParent();
+        // set the child pointer of node's parent as nil
+        parent.setChild(null);
+        // set the parent pointer of the node as nil
+        node.setParent(null);
+        // if the node has siblings update their pointers before removing the node
+        if (node.getLeftSibling() != node || node.getRightSibling() != node) {
+            Node leftSibling = node.getLeftSibling();
+            Node rightSibling = node.getRightSibling();
+            leftSibling.setRightSibling(rightSibling);
+            rightSibling.setLeftSibling(leftSibling);
+        }
+        node.setLeftSibling(node);
+        node.setRightSibling(node);
+        node.setChildCut(false);
+        // re-insert node (and its's subtree) into the heap
+        addNode(node);
+        // Check to perform cascading cut only if the node's parent is not a root
+        if(parent.getParent() != null) {
+            // if childCut if already true, remove and re-insert the parent into the heap
+            // else set childCut of parent to true
+            if(parent.isChildCut()) {
+                removeAndReinsertNode(parent);
             } else {
-                node.setCount(node.getCount() + frequency);
+                parent.setChildCut(true);
             }
         }
     }
